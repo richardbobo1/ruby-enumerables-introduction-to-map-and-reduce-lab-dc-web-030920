23a3d2f1a564289311eef7b2ce79ffa2597add0c
Richard Bobo <richard.bobo@gmail.com>
2020-03-07 17:53:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,46 @@
-# My Code here....
+def map_to_negativize(source_array)
+ source_array.map { |i| -i }
+end
+
+
+def map_to_no_change(source_array)
+  source_array.map { |i| i }
+end
+
+
+def map_to_double(source_array)
+  source_array.map { |i| i*2 }
+end
+
+def map_to_square(source_array)
+  source_array.map { |i| i**2 }
+end
+
+
+def reduce_to_total(source_array, starting_point = 0 )
+  source_array.reduce(starting_point) { |sum, n| sum + n } 
+end
+
+
+
+
+def reduce_to_all_true(source_array)
+  
+  counter = 0 
+  while counter < source_array.length do 
+    if !source_array[counter] 
+      return false 
+
+   end 
+        counter += 1
+  
+end
+       return true
+
+end 
+
+
+
+def reduce_to_any_true(source_array)
+  source_array.reduce {|i| i}
+end
